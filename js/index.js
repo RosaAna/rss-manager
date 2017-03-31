2d196c9873dab7da9ce0bd28deb3a853abdc25cc
Jose María Ruano Gálvez <Jose Mara Ruano Glvez>
2017-03-31 20:46:09 +0200

    Añadida página de lista, añadidas animaciones que la acompañan. 	Comenzado el popup para añadir más fuentes RSS o Atom a la 	aplicación.
    	TODO - Se encuentra en el archivo index.html y index.js

--- js/index.js
+++ js/index.js
@@ -3,27 +3,68 @@
 
 $(document).ready(function() {
     
+    // Objeto que detecta si la página está siendo cargada desde un dispositivo móvil.    
+    var isMobile = {
+        Android: function() {
+            return navigator.userAgent.match(/Android/i);
+        },
+        BlackBerry: function() {
+            return navigator.userAgent.match(/BlackBerry/i);
+        },
+        iOS: function() {
+            return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+        },
+        Opera: function() {
+            return navigator.userAgent.match(/Opera Mini/i);
+        },
+        Windows: function() {
+            return navigator.userAgent.match(/IEMobile/i);
+        },
+        any: function() {
+            return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+        }
+    };
+  
     $.feed.cargaNoticia("http://www.ideal.es/jaen/rss/2.0/portada", "#caja1", "rss");
     
+    // Ocultamos todos los elementos que no sean la página principal de la aplicación
+    $(".listpage").hide();
+    
+    // Acción que se realiza para animar los elementos cuando hacemos clic en un botón.
     $(".list").click(function() {
-        $(".flex-icons").slideUp();
-            
-            setTimeout(function() {
-            
-            $(".listpage").fadeIn();
-            
-            }, 300)
+        $("html, body").animate({
+            scrollTop: 0,
+        }, 600);
         
-        // TODO - ESTILAR DIV DE LISTA DE RSS (NOMBRE-FUENTE, TIPO, AÑADIDA/ACTUALIZADA(FECHA))...
+        $(".flex-icons").fadeOut(400, function() {
+            $(".listpage").fadeIn(400);
+            
+            $(".listheader").animate({
+                height: "50px",
+            }, 400);
+            
+        });
+        
+    });
+    
+    // Efecto contrario para volver a la página principal
+    $(".backarrow").click(function() {    
+        $("html, body").animate({
+            scrollTop: 0,
+        }, 600);
+        
+        $(".listheader").animate({
+            height: "0px",
+        }, 400);
+        
+        $(".listpage").fadeOut(400, function() {
+            $(".flex-icons").fadeIn(400);
+            
+        });
+        
     });
     
 });
-
-function mostrarContenido() {
-        
-    $.afui.loadContent("#pagina1", false, false, "up");
-    
-}
 
 $.feed = {};
 
